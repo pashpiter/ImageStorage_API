bf6fc30fc48c2f5333c06c8c2a97605f3951ff46
Pavel Drovnin <pavel98rus@gmail.com>
2023-06-06 01:20:19 +0300

    update get response

--- views.py
+++ views.py
@@ -2,6 +2,8 @@ from aiohttp import web
 import json
 from PIL import Image
 from io import BytesIO
+
+from multidict import MultiDict
 from db import db_insert, db_select
 
 
@@ -13,14 +15,18 @@ async def post_handler(request):
     #     return web.json_response(data, status=200)
     if request.content_type != 'multipart/form-data':
         return web.Response(status=400, text='Используйте form/data для отправки изображения')
+    for i in range(4):
+        await request.content.readline()
     img = await request.content.read()
     p_img = Image.open(BytesIO(img))
     id_img = await db_insert(img)
     r = {'image_id': id_img}
-    return web.json_response(r, status=204)
+    return web.json_response(r, status=201)
     
 async def get_handler(request):
-    id = int(request.path[1:])
+    id = int(request.match_info['id'])
     db_img = await db_select(id)
     img = Image.open(BytesIO(db_img))
-    return web.Response(text=id)
+    out = BytesIO(db_img)
+    return web.Response(
+        body=out, content_type="image/jpeg")
