8430d60a9e18bed0a0e38b06021fa43efc8c85d6
Pavel Drovnin <pavel98rus@gmail.com>
2023-06-09 15:57:22 +0300

    edit annotation

--- main.py
+++ main.py
@@ -15,37 +15,41 @@ logger = get_logger(__name__)
 @routes.get('/logs')
 async def logs(
             request: web.Request
-        ) -> Callable[[web.Request], web.json_response]:
+        ) -> tuple[web.json_response,
+                   Callable[[web.Request], web.json_response]]:
     """Обработка запроса на получения логов"""
     if not await check_token(request):
-        return web.json_response({'erorr': 'Unauthorized'}, status=401)
+        return web.json_response({'error': 'Unauthorized'}, status=401)
     return await get_logs(request)
 
 
 @routes.get('/{id}')
 async def get(
             request: web.Request
-        ) -> Callable[[web.Request], web.Response]:
+        ) -> tuple[web.json_response, Callable[[web.Request],
+                                               tuple[web.Response,
+                                                     web.json_response]]]:
     """Обработка GET-запросов c ID изображения"""
     if not await check_token(request):
-        return web.json_response({'erorr': 'Unauthorized'}, status=401)
+        return web.json_response({'error': 'Unauthorized'}, status=401)
     return await get_handler(request)
 
 
 @routes.post('/')
 async def post(
             request: web.Request
-        ) -> Callable[[web.Request], web.json_response]:
+        ) -> tuple[web.json_response,
+                   Callable[[web.Request], web.json_response]]:
     """Обработка POST-запросов"""
     if not await check_token(request):
-        return web.json_response({'erorr': 'Unauthorized'}, status=401)
+        return web.json_response({'error': 'Unauthorized'}, status=401)
     return await post_handler(request)
 
 
 @routes.post('/auth/token')
 async def auth_get_token(
             request: web.Request
-        ) -> Callable[[web.Request], web.json_response]:
+        ) -> tuple[web.Response, Callable[[str, str], web.json_response]]:
     _json_dict = await request.json()
     username, password = _json_dict.get('username'), _json_dict.get('password')
     if not username or not password:
@@ -53,7 +57,7 @@ async def auth_get_token(
             status=400,
             text='Для получения токена нужно ввести имя пользователя и пароль'
         )
-    return await create_token(username, password)
+    return await create_token(username, password, request.host+request.path)
 
 if __name__ == '__main__':
     app.add_routes(routes)
--- views.py
+++ views.py
@@ -10,21 +10,27 @@ from logger_app import get_logger
 logger = get_logger(__name__)
 
 
-async def get_handler(request: web.Request) -> web.Response:
+async def get_handler(request: web.Request) -> tuple[web.Response,
+                                                     web.json_response]:
     """Обрабатываем GET-запросы"""
     id = int(request.match_info['id'])
     logger.info(
         f'Получен GET-запрос с ID={id}',
         {'route': request.host+request.path}
     )
+    if id < 1:
+        return web.json_response(
+            status=400,
+            data={'error': 'ID должен быть больше 0'})
     db_bytes_img = await db_select(id)
     if not db_bytes_img:
         logger.error(
             f'Изображение с ID={id} в базе не найдено',
             {'route': request.host+request.path}
         )
-        return web.Response(
-            status=404, text=f'Изображение с ID={id} в базе не найден')
+        return web.json_response(
+            status=404,
+            data={'error': f'Изображение с ID={id} в базе не найден'})
     bytes_img = BytesIO(db_bytes_img)
     return web.Response(
         body=bytes_img.getvalue(), content_type="image/jpeg")
@@ -38,8 +44,9 @@ async def post_handler(request: web.Request) -> web.json_response:
             'Передан неподходящий контент',
             {'route': request.host+request.path}
         )
-        return web.Response(
-            status=400, text='Используйте form/data для отправки изображения')
+        return web.json_response(
+            status=400,
+            data={'error': 'Используйте form/data для отправки изображения'})
     for i in range(4):
         await request.content.readline()
     bytes_img = await request.content.read()
@@ -49,16 +56,16 @@ async def post_handler(request: web.Request) -> web.json_response:
         heigth = int(request.query.get('y', img.height))
         quality = int(request.query.get('quality', 100))
         if quality and not 0 < quality <= 100:
-            return web.Response(
-                status=400, text='Quality не может быть меньше 0 и больше 100')
+            return web.json_response(
+                status=400,
+                data={'error': 'Quality не может быть меньше 0 и больше 100'})
         img = img.resize((width, heigth))
     if img.format != 'JPEG':
         img = img.convert('RGB')
     output = BytesIO()
     img.save(output, format='JPEG', quality=quality)
     id_img = await db_insert(output.getvalue())
-    data = {'image_id': id_img}
-    return web.json_response(data, status=201)
+    return web.json_response(data={'image_id': id_img}, status=201)
 
 
 async def get_logs(request: web.Request) -> web.json_response:
