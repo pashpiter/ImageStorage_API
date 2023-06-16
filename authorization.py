9c96d2b70ed70eb5b87b21744281835e16ff275b
Pavel Drovnin <pavel98rus@gmail.com>
2023-06-16 06:26:28 +0300

    edit auth

--- authorization.py
+++ authorization.py
@@ -1,3 +1,6 @@
+import io
+from typing import Callable
+
 import jwt
 import yaml
 from aiohttp import web
@@ -10,9 +13,32 @@ allowed_tokens_path = BASE_DIR / 'config' / 'allowed_tokens.yaml'
 logger = get_logger(__name__)
 
 
+async def add_or_return_token(
+        username: str, token: str, password: str,
+        f: io.TextIOWrapper, route: str, key: str
+) -> web.json_response:
+    """Добавляет токен для пользователя в разрешенные или
+    возвращает токен ддля пользователя созданный до этого"""
+    parsed_tokens = yaml.safe_load(f)
+    if (
+        parsed_tokens and username not in parsed_tokens
+    ) or not parsed_tokens:
+        yaml.dump({username: token}, f)
+        logger.info(f'Добавление нового токена для {username}',
+                    {'route': route})
+        return web.json_response(data={'token': token}, status=201)
+    else:
+        if jwt.decode(
+            parsed_tokens[username], key, algorithms="HS256"
+        )['password'] != password:
+            return web.json_response(
+                data={'error': 'Неверный пароль'}, status=403)
+        return web.json_response(data={'token': token}, status=200)
+
+
 async def create_token(
         username: str, password: str, route: str
-) -> web.json_response:
+) -> Callable[[str, str, str, io.TextIOWrapper, str, str], web.json_response]:
     """Получение токена для пользователя"""
     logger.info(f'Получен POST-запрос от {username}',
                 {'route': route})
@@ -22,22 +48,14 @@ async def create_token(
         'password': password
     }
     token = jwt.encode(payload, key)
-    with open(allowed_tokens_path, 'w+') as f:
-        parsed_tokens = yaml.safe_load(f)
-        if (
-            parsed_tokens and username not in parsed_tokens
-        ) or not parsed_tokens:
-            yaml.dump({username: token}, f)
-            logger.info(f'Добавление нового токена для {username}',
-                        {'route': route})
-            return web.json_response(data={'token': token}, status=201)
-        else:
-            if jwt.decode(
-                parsed_tokens[username], key, algorithms="HS256"
-            )['password'] != password:
-                return web.json_response(
-                    data={'error': 'Неверный пароль'}, status=403)
-            return web.json_response(data={'token': token}, status=200)
+    try:
+        with open(allowed_tokens_path, 'r+') as f:
+            return await add_or_return_token(username, token, password,
+                                             f, route, key)
+    except Exception:
+        with open(allowed_tokens_path, 'w+') as f:
+            return await add_or_return_token(username, token, password,
+                                             f, route, key)
 
 
 async def check_token(request: web.Request) -> bool:
